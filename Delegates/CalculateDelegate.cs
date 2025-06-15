8721fb04fb307380838bc9dc59d8f174d9f4a2bb
Lara Pereira <larabernspereira@gmail.com>
2025-06-15 16:56:41 -0300

    add exer 1

---
+++ Delegates/CalculateDelegate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AT.Delegates
+{
+
+    public delegate double CalculateDiscount(double valor);
+
+    public class DiscountCalculator
+    {
+        public static double CalculateDiscountMethod(double valor)
+        {
+            return valor * 0.9;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o preço original de uma diária: ");
+
+            if (double.TryParse(Console.ReadLine(), out double precoDiaria))
+            {
+                CalculateDiscount handler = DiscountCalculator.CalculateDiscountMethod;
+
+                double precoComDesconto = handler(precoDiaria);
+
+                Console.WriteLine($"O preço com desconto da diária é: R$ {precoComDesconto:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido! Por favor, digite um número.");
+            }
+        }
+    }
+}

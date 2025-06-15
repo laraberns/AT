9a9f90ea4baaeeb694e629f7ad81fdbee00008cf
Lara Pereira <larabernspereira@gmail.com>
2025-06-15 19:06:47 -0300

    add exer 9

---
+++ ATContext.cs
@@ -0,0 +1,22 @@
+﻿using AT.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AT.Data
+{
+    public class ATContext : DbContext
+    {
+        public ATContext(DbContextOptions<ATContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Destino> Destinos { get; set; }
+        public DbSet<PacoteTuristico> PacotesTuristicos { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,15 +10,14 @@ namespace AT
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            builder.Services.AddDbContext<ATContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<ATContext>();
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
